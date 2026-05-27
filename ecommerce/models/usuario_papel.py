14fb199f9285a1511f6055b9a08986d7cb019e5b
ZagZx <azevedodvictor@gmail.com>
2026-05-27 08:14:07 -0300

    default=None removido dos campos

--- ecommerce/models/usuario_papel.py
+++ ecommerce/models/usuario_papel.py
@@ -1,8 +1,7 @@
-from typing import Optional
 from sqlmodel import SQLModel, Field
 
 class UsuarioPapel(SQLModel, table=True):
     __tablename__ = "usuario_papeis"
 
-    usuario_id: Optional[int] = Field(default=None, foreign_key="usuarios.id", primary_key=True)
-    papel_id: Optional[int] = Field(default=None, foreign_key="papeis.id", primary_key=True)
+    usuario_id: int = Field(foreign_key="usuarios.id", primary_key=True)
+    papel_id: int = Field(foreign_key="papeis.id", primary_key=True)
