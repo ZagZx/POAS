a879a7723e7f2a9e7ec5a70cd87cb6578e911a50
ZagZx <azevedodvictor@gmail.com>
2026-06-01 08:23:46 -0300

    decodificando token para pegar o usuario

--- ecommerce/routes/login.py
+++ ecommerce/routes/login.py
@@ -4,7 +4,10 @@ from fastapi import (
     status,
     Depends
 )
-from fastapi.security.oauth2 import (OAuth2PasswordRequestForm)
+from fastapi.security.oauth2 import (
+    OAuth2PasswordRequestForm,
+    OAuth2PasswordBearer
+)
 from sqlmodel import select, col
 from typing import Annotated
 from datetime import timedelta, datetime
@@ -18,12 +21,14 @@ from models import Usuario
 
 load_dotenv()
 
+oauth_schema = OAuth2PasswordBearer(tokenUrl="token")
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 
 def create_acess_token(data: dict, expires: timedelta = None):
     to_encode = data.copy()
-    
+
     if expires:
         expires_date = datetime.now() + expires
     else:
@@ -33,6 +38,31 @@ def create_acess_token(data: dict, expires: timedelta = None):
     jwt_encode = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return jwt_encode
+
+def get_usuario(token: Annotated[str, Depends(oauth_schema)], session: SessionDep) -> Usuario:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Email ou senha incorreta",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+
+    try:
+        dados = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email = dados["sub"]
+        if not email:
+            raise credentials_exception
+        
+        usuario = session.scalar(
+            select(Usuario).where(
+                Usuario.email == email
+            )
+        )
+        if not usuario:
+            raise credentials_exception
+        
+        return usuario
+    except Exception:
+        raise credentials_exception
 
 login_router = APIRouter(prefix="/login", tags=["Login"])
 
@@ -48,10 +78,14 @@ def login(
     )
 
     if not usuario:
-        raise HTTPException(400, "Email ou senha incorreta")
+        raise HTTPException(
+            status.HTTP_401_UNAUTHORIZED, 
+            "Email ou senha incorreta")
     
     if not check_password_hash(form_data.password, usuario.senha_hash):
-        raise HTTPException(400, "Email ou senha incorreta")
+        raise HTTPException(
+            status.HTTP_401_UNAUTHORIZED, 
+            "Email ou senha incorreta")
     
     access_token = create_acess_token({"sub": usuario.email})
 
